439d44d7b8743d42ccdd56e07c400d63d57b3a51
Viktoriia <48953625+ViKuzya@users.noreply.github.com>
2020-11-16 21:49:27 +0200

    Update Test Tasck Poliashchenko Viktoriia.js

--- Test Tasck Poliashchenko Viktoriia.js
+++ Test Tasck Poliashchenko Viktoriia.js
@@ -2,12 +2,12 @@
 
 describe ('Test Task after CV ', () => {
     it('Сheck list of available places', () => {
-        cy.visit('https://www.booking.com/') //Visit URL
+        cy.visit('https://www.booking.com/')
         cy.url().should('include', 'booking') //Verify URL 
 
-        cy.get('[data-component="search/destination/input"]').click();
-        cy.get('[data-i="1"]').click();
-        cy.get('[data-date="2020-12-09"]').click();
+        cy.get('[data-component="search/destination/input"]').click(); 
+        cy.get('[data-i="1"]').click(); //Сhoose location
+        cy.get('[data-date="2020-12-09"]').click(); //Сhoose data
         cy.get('[data-date="2020-12-16"]').click();
 
         cy.get('button[type="submit"]').should('be.enabled').first().click(); //Search available places
