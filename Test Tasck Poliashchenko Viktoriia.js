05c6309a6a49a3c4e2758fafb98437fc96fd442e
Viktoriia <48953625+ViKuzya@users.noreply.github.com>
2020-11-16 21:46:40 +0200

    Add files via upload

---
+++ Test Tasck Poliashchenko Viktoriia.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />
+
+describe ('Test Task after CV ', () => {
+    it('Сheck list of available places', () => {
+        cy.visit('https://www.booking.com/') //Visit URL
+        cy.url().should('include', 'booking') //Verify URL 
+
+        cy.get('[data-component="search/destination/input"]').click();
+        cy.get('[data-i="1"]').click();
+        cy.get('[data-date="2020-12-09"]').click();
+        cy.get('[data-date="2020-12-16"]').click();
+
+        cy.get('button[type="submit"]').should('be.enabled').first().click(); //Search available places
+        cy.url().should('include', '/searchresults')//Verify search results
+        
+    })
+})
